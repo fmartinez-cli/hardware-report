9a08b8397db50393f136303077dc7887a97c1dd6
fmartinez-cli <fernandomtz.barbosa@gmail.com>
2026-03-15 05:05:57 -0700

    update footer

--- header_footer/footer.php
+++ header_footer/footer.php
@@ -11,15 +11,10 @@
 
 <!----------------------------------------- inicia el footer---------------------------------------------------------------------------------------->
 
-<footer id="sticky-footer" class="text-center text-white" style="background-color: #21081a;margin-top: 100 px;margin-top: 5%;">
- 
-
-  <!-- Copyright -->
+<footer id="sticky-footer" class="text-center text-white" style="background-color: #21081a; margin-top: 5%;">
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    &#169 2021 Copyright:
-    <a href="mailto:fernando.martinez@fii-na.com">LVO Bahia 06</a><!-- Enlace al correo electronico -->
+    &copy; <?php echo date('Y'); ?> <?php echo defined('APP_FOOTER') ? APP_FOOTER : 'Hardware Report System'; ?>
   </div>
-  <!-- Copyright -->
 </footer>
 
 <!-------------------------------------------- acaba el footer ------------------------------------------------------------------------------------------>
